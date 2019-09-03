79276559b1109c1917942561e4e1ee89c9d33480
Santi Gracia <santi.gracia@mixpanel.com>
2019-09-03 10:21:16 -0700

    Initial Commit

---
+++ musicfinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  musicfinder
+//
+//  Created by Santi Gracia on 9/3/19.
+//  Copyright © 2019 Mixpanel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
